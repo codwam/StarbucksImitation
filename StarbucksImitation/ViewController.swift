a7340aa6bd34a19a92143c0f1116fda53ff0f1bf
codwam <codwam@gmail.com>
2016-06-17 02:33:16 +0800

    Initial Commit

---
+++ StarbucksImitation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StarbucksImitation
+//
+//  Created by lh on 16/6/17.
+//  Copyright © 2016年 codwam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
